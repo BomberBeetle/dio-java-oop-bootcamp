25dee7724bc3135629e50c2a99d0ce18ea197397
BomberBeetle <gdreks@gmail.com>
2022-01-20 21:53:09 -0300

    hep2

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,39 @@
+package br.com.dio.desafio.dominio;
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+
+    }
+
+    public void progredir(){
+
+    }
+
+    public void calcularXp(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+}
